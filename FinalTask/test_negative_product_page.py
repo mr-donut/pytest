485ec1beac3dcb17b96ca23c3c6f14d1c29d4f0e
mr-donut <qwee001@mail.ru>
2020-01-17 21:49:20 +0300

    добавление новых тестовб в т.ч. параметризация и негативные

--- FinalTask/test_negative_product_page.py
+++ FinalTask/test_negative_product_page.py
@@ -1,21 +1,25 @@
 from .pages.product_page import ProductPage
+import time
 
-def test_guest_should_see_add_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+def test_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
 
 def test_guest_should_see_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
-    browser.implicitly_wait(2)
-    page.add_to_basket()
-    page.solve_quiz_and_get_code()
-    browser.implicitly_wait(5)
-    page.should_be_message()
-    page.compare_the_basket_price()
+    time.sleep(1)
+    page.is_disappeared()
--- FinalTask/test_product_page.py
+++ FinalTask/test_product_page.py
@@ -1,14 +1,18 @@
 from .pages.product_page import ProductPage
+from .pages.base_page import BasePage
+import pytest
 
+@pytest.mark.skip
 def test_guest_should_see_add_link(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+
     page = ProductPage(browser, link)
     page.open()
 
+@pytest.mark.skip
 def test_guest_should_see_message(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
@@ -19,3 +23,17 @@ def test_guest_should_see_message(browser):
     browser.implicitly_wait(5)
     page.should_be_message()
     page.compare_the_basket_price()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    login_link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = BasePage(browser, login_link)
+    page.open()
