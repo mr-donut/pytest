77c887e86774522dde69dfc4d61a5f569c58393c
mr-donut <qwee001@mail.ru>
2020-01-19 16:16:22 +0300

    Добавлена проверка корзины

---
+++ FinalTask/pages/basket_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from .locators import BasketPageLocators
+from selenium.webdriver.common.by import By
+
+class BasketPage(BasePage):
+    def go_to_basket_link(self):
+        basket_link = self.browser.find_element(*BasketPageLocators.BASKET_LINK)
+        basket_link.click()
+        print("Зашли в корзину")
+        return BasketPage(browser=self.browser, url=self.browser.current_url)
+
+    def should_be_goods_absence(self):
+        assert not self.is_element_present(*BasketPageLocators.PROCCED_LINK), "Basket not empty!"
+
+    def basket_empty_message(self):
+        language = self.browser.execute_script("return window.navigator.userLanguage || window.navigator.language")
+        # page_language = self.browser.find_element(By.CSS_SELECTOR, "option[selected='selected']").text
+        message = self.browser.find_element(By.CSS_SELECTOR, "#content_inner p").text
+
+        assert str("basket is empty") in message, "Basket is not empty."
+        assert str("basket is empty") in message, "Basket is not empty." if language == str("en") else "Language of the page not English!"
+        # assert str("корзина пуста") in message, "Корзина не пуста." if language == str("ru") else "Язык не русский!"
