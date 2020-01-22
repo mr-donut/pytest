f011003602ec341b7c1f184d8c7423d7a85f9aa6
mr-donut <qwee001@mail.ru>
2020-01-22 18:26:32 +0300

    промежуточое состояние

--- FinalTask/pages/locators.py
+++ FinalTask/pages/locators.py
@@ -2,7 +2,10 @@ from selenium.webdriver.common.by import By
 
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class LoginPagesLocators:
     FORM_REGISTER = (By.CSS_SELECTOR, "#register_form")
@@ -12,7 +15,12 @@ class LoginPagesLocators:
 class ProductPageLocators:
     ADD_LINK = (By.CSS_SELECTOR, "#add_to_basket_form.add-to-basket")
 
-    BASKET_LINK = (By.CSS_SELECTOR, "#content_inner")
+    BASKET_LINK = (By.CSS_SELECTOR, "span.btn-group")
 
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div.alert:nth-child(1)")
     #"div.alert.alert-safe.alert-noicon.alert-success.fade.in div.alertinner"
+
+class BasketPageLocators:
+    BASKET_LINK = (By.CSS_SELECTOR, "span.btn-group")
+
+    PROCCED_LINK = (By.CSS_SELECTOR, ".btn-primary.btn-block")
